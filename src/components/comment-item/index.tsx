138ce1eba498c839977293593bf3ed5cac5bf2db
mahdi morasaeiyan <mahdimoras@gmail.com>
2023-01-30 19:17:02 +0330

    add:metionHeliht add

--- src/components/comment-item/index.tsx
+++ src/components/comment-item/index.tsx
@@ -25,6 +25,15 @@ const CommentsItem = ({
 
   const [reply, setReply] = useState(false);
 
+  const mention = () => {
+    let regex = /@\w+[\s|$|\?]/;
+    let matches = text.replace(
+      regex,
+      '<sapn style="background-color: #e9effd; color:#2b64e8;padding:3px 5px ">$&</sapn>'
+    );
+    return matches;
+  };
+
   return (
     <div className={`${style.container} ${replies && style.border}`}>
       <div className={style.box_avatar}>
@@ -37,7 +46,10 @@ const CommentsItem = ({
           <h3>{user?.name}</h3>
           <TimeAgo date={date} />
         </div>
-        <p className={style.text}>{text}</p>
+        <p
+          className={style.text}
+          dangerouslySetInnerHTML={{ __html: mention() }}
+        />
         <div className={style.button_container}>
           <Like iLikedIt={iLikedIt} likes={likes} />
           {replies && (
